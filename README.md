b32d23b81df0f652ec45154934a383fbc953f1e1
Franklin Rice <frice@berkeley.edu>
2016-11-06 15:35:44 -0800

    Basic Templates and readme
    Created templates for main controller, header, labels, and modals. All will need customization. Created README and guidelines for contributing to it

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Group-08-Project
-Group 08 Project
+# CS160 Group 8 (Team Hufflepuff) V1 coded prototype
+
+## Use this readme to post tips for other group members to ease development. For example:
+* Tempates you created, how they can be created, and what parameters they must take (sample code is great)
+* Other protocalls/functions that are helpful or necessary to know (basically so that nobody ever has to dig through code they didn't write looking for exactly what arguments they need to pass into something)
+* Design specifications/generalizations/guidelines we should follow, for consistency
+* Project roadmap and other important notes. I imagine much of this can go in the facebook chat as well
+
+## Templates, models, and other functions
+
+new header() - hard coded for main screen. Can accept parameters but doesn't use them. This should be changed to use params if we want different headers for each page
+
+new Modal({string: "some string"}) - a simple modal window that pops up in the middle of the screen. Position must be hard coded for screen size. Default confirmation string is currently "Okay!"
+
+new lbl({str: "some string", style: someStyle, touchFxn: someFxn}) - just put the uncalled function here and define it globally. Use nullFxn if you don't want any touch behavior
+
+## Design Guidelines
+
+### Styles
+* basicTextStyle: for normal text (14px black)
+* headerTextStyle: for header use (18 black)
+* elemTextStyle: for text in elements (14 gray)
+* linkTextStyle: same as elemText only blue and italic
+
+### General Design 
+* [Apple design Guidelines](https://designcode.io/iosdesign-guidelines)
+* [Readme markdown guide](https://daringfireball.net/projects/markdown/basics)
+
+
+## Roadmap
---
+++ main.js
@@ -0,0 +1,151 @@
+/*
+ *     Copyright (C) 2010-2016 Marvell International Ltd.
+ *     Copyright (C) 2002-2010 Kinoma, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+import Pins from "pins";
+import {VerticalScroller} from "scroller"; 
+//TODO This is not the best scroller, it works but it covers up the header bar
+// Consider finding a better scroller if we need it (I'm not sure we do)
+// Scroller is only necessary for the calendar I believe
+
+let basicTextStyle = new Style({ font: "14px", color: "black" });
+let headerTextStyle = new Style({ font: "bold italic 18px", color: "white"});
+let elemTextStyle = new Style({ font: "14px", color: "#3c4241"});
+let elemLinkStyle = new Style({ font: "italic 14px", color: "#007aff"}); //this is the apple Link color
+
+
+let MainContainer = Column.template($ => ({
+    top: 0, bottom: 0, left: 0, right: 0,
+    skin: new Skin({ fill: "#efeff4" }), //default Apple background
+    contents: [
+        new header(),
+        new VerticalScroller($, {
+            name: 'scroller',
+            contents: [
+                new Column({
+                    top:0,left:0,right:0,
+                    contents: [
+                        // TODO INSERT SCROLLING PAGE CONTENT HERE
+
+                    ]
+                })
+            ]
+        })
+    ],
+}));
+
+let header = Line.template($ => ({
+    top:0, left:0, right:0,
+    skin: new Skin({ fill: "007aff"}), //Apple safari skin, videos use #5ac8fa
+    contents: [
+        new Container({
+            top:0,left:0,right:0,bottom:0,
+            contents: [
+            new Label({
+                string: "Left",
+                top:5,
+                style: headerTextStyle,
+                horizontal: "left",
+                active:true,
+                behavior: Behavior({
+                    onTouchEnded(content,id,x,y,ticks) {
+                        //TODO what to do when left header clicked
+                    }
+                })
+            }),
+            ]
+        }),
+        new Container({
+            top:0,left:0,right:0,bottom:0,
+            name: "title",
+            contents: [
+            new Label({
+                string: "Center header",
+                top:5,
+                style: headerTextStyle
+            }),
+            ]
+        }),
+        new Container({
+            top:0,left:0,right:0,bottom:0,
+            contents: [
+            new Label({
+                string: "Right",
+                top:5,
+                style: headerTextStyle,
+                horizontal: "right",
+                active:true,
+                behavior: Behavior({
+                    onTouchEnded(content,id,x,y,ticks) {
+                        //TODO what to do when right header clicked
+                    }
+                })
+            }),
+            ]
+        })
+    ]
+}));
+
+//create a label with label = new lbl({str: "some string", style: someStyle, touchFxn: uncalledFxn (if any)}) 
+// Also include params to the touch function in the label params
+let lbl = Label.template($ => ({
+    left:10,
+    string: $.str,
+    style: $.style,
+    active: true,
+    behavior: Behavior({
+        onTouchEnded(content, id, x,y,ticks) {
+            $.touchFxn($)
+        }
+    })
+}));
+
+function nullFxn() {
+	//do nothing
+}
+
+//TODO generally customize this modal window (size, position, message, style)
+let Modal = Column.template($ => ({
+    bottom: 200,//TODO change this value as desired
+    skin: new Skin({fill:"white"}),
+    contents: [
+        new Container({
+            height:50, width:200, //TODO change values
+            skin: new Skin({fill:"white"}),
+            contents: [
+                new Label({string: $.string, style: blackTextStyle}),
+
+            ]
+        }),
+        new Line({
+            height:40,width:200, //TODO change values
+            skin: new Skin({fill:"blue"}),
+            active: true,
+            contents: [
+                new Label({string: "Okay!", style: headerTextStyle, left:0,right:0})
+                //TODO change this string
+            ],
+            behavior: Behavior({
+                onTouchEnded(content,id,x,y,ticks) {
+                	//TODO what to do when bar at bottom of modal is clicked
+                }
+            })
+        }),
+    ]
+}));
+
+
+let mc = new MainContainer({});
+application.add(mc);
